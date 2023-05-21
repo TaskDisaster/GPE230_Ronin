661c342cddb20cfd404d3a4be8b91c9018419dc6
Ronin <RoninL05140130@gmail.com>
2023-05-20 21:50:12 -0700

    testina

---
+++ Source/Project205/Project205GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Project205GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT205_API AProject205GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
